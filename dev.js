c05375dea202c6521d18b73c4b970a5c8a38e686
Daniel Bornstrand <daniel.bornstrand@gloot.com>
2019-05-28 18:33:14 +0200

    first commit

---
+++ dev.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const chokidar = require('chokidar');
+const clearModule = require('clear-module');
+const service = require('./lib/service.js');
+
+async function init() {
+  let build = require('./lib/build');
+  const response = await service.getEntries();
+  const posts = response.items;
+  build({ posts });
+  const watcher = chokidar
+    .watch('.', { ignoreInitial: true, ignored: 'public' })
+    .on('all', (event, path) => {
+      clearModule.all();
+      let build = require('./lib/build');
+      console.info('rebuilding...');
+      build({ posts });
+    });
+  watcher
+    .on('add', path => console.log(`File ${path} has been added`))
+    .on('change', path => console.log(`File ${path} has been changed`))
+    .on('unlink', path => console.log(`File ${path} has been removed`));
+}
+
+init();
