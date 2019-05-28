d9489da03f57d990bb417329e101eab4455438c8
Daniel Bornstrand <daniel.bornstrand@gloot.com>
2019-05-28 20:45:22 +0200

    Wrap whole build process in try/catch

--- build.js
+++ build.js
@@ -3,10 +3,15 @@ const service = require('./lib/service.js');
 
 async function init() {
   console.log('Building static site...');
-  let build = require('./lib/build');
-  const response = await service.getEntries();
-  const posts = response.items;
-  build({ posts });
+  try {
+    let build = require('./lib/build');
+    const response = await service.getEntries();
+    const posts = response.items;
+    await build({ posts });
+    console.log('Site successfully built');
+  } catch (e) {
+    console.error('Error while building site:', e);
+  }
 }
 
 init();
