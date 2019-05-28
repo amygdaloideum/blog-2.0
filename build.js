41a26a44dbfda51a6dca3cd73f5e6750c8416e8d
Daniel Bornstrand <daniel.bornstrand@gloot.com>
2019-05-28 18:56:43 +0200

    Added log when building

--- build.js
+++ build.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const service = require('./lib/service.js');
 
 async function init() {
+  console.log('Building static site...');
   let build = require('./lib/build');
   const response = await service.getEntries();
   const posts = response.items;
