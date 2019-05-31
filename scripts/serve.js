ef4bd9ba93da8003d3db48c3112a1ef32559188d
Daniel Bornstrand <daniel.bornstrand@gloot.com>
2019-05-31 14:00:30 +0200

    Updated port on server

--- scripts/serve.js
+++ scripts/serve.js
@@ -1,13 +1,13 @@
 const handler = require('serve-handler');
 const http = require('http');
 const { join } = require('path');
+
+const PORT = process.env.PORT || 8080;
  
 const server = http.createServer((request, response) => {
-  // You pass two more arguments for config and middleware
-  // More details here: https://github.com/zeit/serve-handler#options
   return handler(request, response, { public: join(__dirname, '../public')});
 })
  
-server.listen(3000, () => {
-  console.log('Running at http://localhost:3000');
+server.listen(PORT, () => {
+  console.log(`Running at http://localhost:${PORT}`);
 });
