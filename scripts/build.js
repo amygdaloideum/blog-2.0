3454dd277c36425a024731cc521f1019f3b55a3e
Daniel Bornstrand <daniel.bornstrand@gmail.com>
2020-12-12 20:07:09 +0100

    Added better build logs

--- scripts/build.js
+++ scripts/build.js
@@ -1,16 +1,24 @@
 require('dotenv').config();
+require('colors');
 const service = require('../lib/service.js');
 const build = require('../lib/make-site');
 
 async function init() {
-  console.log('Building static site...');
+  console.log('Initiating static site build...'.magenta);
   try {
+    console.log('Fetching posts...'.magenta);
     const response = await service.getEntries();
     const posts = response.items;
+    console.log('Posts successfully fetched.\n'.magenta);
+    console.log('The following posts were fetched:'.bgMagenta.black);
+    console.log('\n');
+    posts.forEach((post) =>
+      console.log(`- ${post.fields.title}`.bgYellow.black)
+    );
     await build({ posts });
-    console.log('Site successfully built');
+    console.log('\nSite successfully built'.green);
   } catch (e) {
-    console.error('Error while building site:', e);
+    console.error('Error while building site:'.red, e);
   }
 }
 
